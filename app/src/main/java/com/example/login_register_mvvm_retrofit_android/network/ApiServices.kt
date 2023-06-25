ed8633ac24958815c0feee74b487b3bb0efc7fb0
Y <hwiyul_g@daum.net>
2023-06-25 23:51:10 +0900

    Add : ApiServices interface

--- app/src/main/java/com/example/login_register_mvvm_retrofit_android/network/ApiServices.kt
+++ app/src/main/java/com/example/login_register_mvvm_retrofit_android/network/ApiServices.kt
@@ -1,4 +1,12 @@
 package com.example.login_register_mvvm_retrofit_android.network
 
+import com.example.login_register_mvvm_retrofit_android.model.LoginReq
+import com.example.login_register_mvvm_retrofit_android.model.LoginRes
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ApiServices {
+    @POST("/login")
+    fun login(@Body loginReq: LoginReq): Call<LoginRes>
 }
